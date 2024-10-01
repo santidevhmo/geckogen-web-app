e22e79405abbdffb4b5b4672d0aa588fe1917c44
santidevhmo <santiagoagdc42@gmail.com>
2024-10-01 10:18:59 -0700

    add new navbar option

---
+++ app/components/NavBar/utils/MorphMarketBtn.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const MorphMarketButton = () => {
+    return (
+        <div className="relative group inline-block">
+            <Link href="https://www.morphmarket.com/" passHref>
+                <button className="border-2 border-black rounded-lg py-1.5 px-2" style={{ width: '100px', height: '35px' }}>
+                    <img
+                        src="/morph-market-logo.webp"
+                        alt="Morph Market Logo"
+                        className="w-full h-full object-contain"
+                    />
+                </button>
+                {/* Tooltip */}
+                <div className="absolute bottom-[-50px] left-1/2 transform -translate-x-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-white text-black text-xs rounded-md py-4 px-4 shadow-lg">
+                    A marketplace of trusted breeders and reptile enthusiasts.
+                </div>
+            </Link>
+        </div>
+    );
+};
+
+export default MorphMarketButton;
